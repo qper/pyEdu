45bfa34680b53429a30ceb62f3dd8737aa29606d
Aleksei Smirnov <alsmirnov@luxoft.com>
2020-05-04 01:47:42 +0200

    [CFG] Print a datastructure to output

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ yaml_data = {}
 with open(r'yaml/main.yml') as file:
     yaml_data = load(file, Loader=FullLoader)
 
-# print(dump(yaml_data))
+print(dump(yaml_data))
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 template = env.get_template('index.html')
 output = template.render(data=yaml_data['person'])
 
-# print(output)
+print(output)
 
 html_file = open("index.html", "w")
 html_file.write(output)
