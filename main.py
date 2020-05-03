d514e077c566f1ab78d7eba2c8bc330cc8573de6
Aleksei Smirnov <alsmirnov@luxoft.com>
2020-05-03 23:10:58 +0200

    [CFG] Add template file

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import yaml
+from yaml import dump, load, FullLoader
+from jinja2 import Environment, FileSystemLoader
 
+yaml_data = {}
 with open(r'yaml/main.yml') as file:
-    print(yaml.dump(yaml.load(file, Loader=yaml.FullLoader)))
+    yaml_data = load(file, Loader=FullLoader)
+
+# print(dump(yaml_data))
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+template = env.get_template('index.html.j2')
+
+output = template.render(data=yaml_data['person'])
+print(output)
