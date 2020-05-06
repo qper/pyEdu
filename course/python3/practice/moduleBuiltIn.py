215a47ccc03118c22454335e377e0fe03baa0b22
qper <asitnn@gmail.com>
2020-05-06 17:35:24 +0200

    [EDU] Add solution for built-in module

---
+++ course/python3/practice/moduleBuiltIn.py
@@ -0,0 +1,17 @@
+import math
+
+task = """
+Импортируйте встроенный модуль math. Вычислите при помощи функций этого модуля:
+1. Корень квадратный из числа 123456789
+2. Факториал числа 987
+3. 876 в степени 54
+"""
+
+
+square = math.sqrt(123456789)
+factorial = math.factorial(987)
+degree = math.pow(876, 54)
+
+print(square)
+print(factorial)
+print(degree)
